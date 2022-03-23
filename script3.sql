5fc1e0c48160d4184a9ddf61d1996d4cc73faad3
alexanderashish <alexanderashish@live.com>
2022-03-23 20:18:54 +0530

    Update script3.sql

--- script3.sql
+++ script3.sql
@@ -1,3 +1,4 @@
 select * from table_name where employee = "name";
 select * from table_name where employee = "abc";
 select * from table_name where employee = "xyz";
+select * from table_name where employee = "xyz";
