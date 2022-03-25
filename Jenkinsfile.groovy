cd1267ca7bd79bb7657ae3359a7f9944444cb3e9
Alex <alexanderashish@live.com>
2022-03-25 20:39:08 +0530

    jenkins file modified

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,6 +3,7 @@ pipeline {
     parameters {
         string(name: 'Username', defaultValue: '' , description: 'Please write your username')
         string(name: 'EmailID', defaultValue: '' , description: 'Please write your email')
+	  password(name: 'Password', defaultValue: '' , description: '')
         choice(name: 'Country', choices:[ 'US','India','Canada','UK','China'] , description: 'Please select your country')
     }
     stages {
